c8c719c90a1d11a102a15c4df7d48f36cd29e45a
asim samuel <asim.samuel1@gmail.com>
2020-05-28 16:02:24 -0400

    completed Looper linked list

--- looper/isloop.js
+++ looper/isloop.js
@@ -1,7 +1,31 @@
 'use strict';
-
+const LinkedList = require('./linkedlist');
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const testt = new LinkedList()
+testt.addToHead(0)
+numbers.forEach(val => testt.addToTail(val))
+testt.getNthNode(6).next = testt.getNthNode(3)
 //Complete this algo
+
 const isLoop = (linkedlist) => {
+  // console.log(linkedlist)
+
+  if (!linkedlist.head || !linkedlist.head.next) {
+    return false
+  }
+  let nodeMap = {}
+  let currentNode = linkedlist.head
+  while (currentNode.next) {
+    if (nodeMap[currentNode.next.value]) {
+      return true
+    } else {
+      nodeMap[currentNode.value] = currentNode.value
+
+    }
+    currentNode = currentNode.next
+  }
+  return false
+
 
 };
 
@@ -14,4 +38,6 @@ This function should return the Node value the loop begins at
 Remember to write some test specs too!
 
 */
+isLoop(testt)
+
 module.exports = isLoop
